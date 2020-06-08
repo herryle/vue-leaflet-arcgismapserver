ff3129b250e4738da4b52247a0870f7fe4316d01
herryle <herryle93@gmail.com>
2020-06-08 23:11:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 在vue项目中基于leaflet和esri-leaflet加载天地图服务和arcgis mapserver服务
+
+## 关键依赖
+
+```shell
+leaflet/  npm  i  leaflet
+esri-leaflet/   npm i esri-leaflet //用于加载arcgis服务
+chinatmsproviders/ npm i leaflet.chinatmsproviders  //用于加载天地图服务  高德地图 谷歌地图 osm地图
+```
+
+## 启动
+
+```
+npm instanll
+npm  run  serve
+```
+
---
+++ app.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.chinatmsproviders'
+import * as esri from 'esri-leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+})
+L.Marker.prototype.options.icon = DefaultIcon
+export default {
+  name: 'Map',
+  data() {
+    return {
+      map: '',
+      map_config: {
+        zoom: 15,
+        center: [31.867316649619237, 117.27596282958986]
+      },
+      optios: {
+        minZoom: 1,
+        maxZoom: 18,
+        key: 'eb35dddb3aa33c7a8fb4218b39d1e424'
+      }
+    }
+  },
+  methods: {
+    initMap() {
+      this.map = L.map('map', this.map_config)
+      L.tileLayer
+        .chinaProvider('GaoDe.Normal.Map', this.options)
+        .addTo(this.map)
+      esri
+        .dynamicMapLayer({
+          url:
+            'http://data.marsgis.cn/arcgis/rest/services/mars/hefei/MapServer/'
+        })
+        .addTo(this.map)
+      this.map.on('click', e => {
+        console.log(e)
+      })
+    }
+  },
+  mounted() {
+    this.initMap()
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+}
+#map {
+  height: 100vh;
+}
+</style>
